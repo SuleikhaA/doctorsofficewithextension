5a7a8c59c1e0477f9a530dfc88167fcdc0a893c6
SuleikhaA <31260562+SuleikhaA@users.noreply.github.com>
2020-09-02 02:15:17 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String patientName= "Stacey Manson";
+        boolean hasInsurance= false;
+        double temp= 102.2;
+        int numVisits= 5;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: "+ patientName);
+        System.out.println("Patient has insurance: "+hasInsurance);
+        System.out.println("Body temperature: " + temp);
+
+        System.out.println("Number of visits: " + numVisits);
+
+    }
+}
